d5a4633cb2168be8c2c12df5b2238c49a934327b
Christopher Carnivale <christopher.carnivale@gmail.com>
2019-06-06 17:35:08 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -14,8 +14,8 @@ const morganOption = (NODE_ENV === 'production')
   : 'common';
 
 app.use(morgan(morganOption));
-app.use(cors());
 app.use(helmet());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello, boilerplate!');
